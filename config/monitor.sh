7daddab58c954a5835797ef9eaf4ab438f7f2dff
Tomas Bjerre <tomasbjerre@yahoo.com>
2013-07-28 18:06:44 +0100

    Fixing

--- config/monitor.sh
+++ config/monitor.sh
@@ -8,9 +8,16 @@ function handle_exit {
 }
 trap handle_exit SIGHUP SIGINT SIGTERM
 
+function check_for_close {
+ if [ $state = "close" ]; then
+  clean_wd
+  rm -rf $cameralock
+  exit
+ fi
+}
+
 function clean_wd {
-  if [ $save_picture = "0" ];
-  then
+  if [ $save_picture = "0" ]; then
    rm -rf $wd/*.jpg
    rm -rf $wd/*.h264
   fi
@@ -23,8 +30,7 @@ function send_file {
 }
 
 function remove_old_images {
- for file in `ls $wd/*image.jpg 2> /dev/null | sort | head -n -$1`;
- do
+ for file in `ls $wd/*image.jpg 2> /dev/null | sort | head -n -$1`; do
   echo "rm $file"
   rm $file
  done
@@ -38,12 +44,11 @@ threshold=3072
 rot=0
 width=640
 height=480
-move_to_webdav=1
+move_webdav=1
 save_movie=1
-max_movie_time=10
+max_movie_time=20
 save_picture=1
-for (( event_num=0 ; ; event_num++ ))
-do
+for (( event_num=0 ; ; event_num++ )) do
  now=`date +"%Y-%m-%d_%H-%M-%S"`
  event=`printf %05d $event_num`
  current="$wd/$event-image.jpg"
@@ -52,47 +57,42 @@ do
  video="$wd/$event-$now-video.h264"
 
  /opt/vc/bin/raspistill -t 0 -n -o $current -w $width -h $height --colfx 128:128 -rot $rot
+ check_for_close
  convert $current -auto-level $current
 
- if [ -e $previous ];
- then
+ if [ -e $previous ]; then
   compare_out="/tmp/compareout"
   compare -metric AE -fuzz 20% $current $previous $diff_file 2> $compare_out
   diff=`cat $compare_out`
 
+  check_for_close
+
   #If motion
-  if [ $diff -gt $threshold ];
-  then
+  if [ $diff -gt $threshold ]; then
    echo "Triggered on $diff"
-   if [ $save_movie = "1" ];
-   then
-    /opt/vc/bin/raspivid -n -t $max_movice_time -o $video  -w $width -h $height -rot $rot
+   if [ $save_movie = "1" ]; then
+    /opt/vc/bin/raspivid -n -t $max_movie_time -o $video  -w $width -h $height -rot $rot
    fi
 
-   if [ $move_webdav = "1" ];
-   then
-    send_file $video
-    send_file $diff_file
-    for file in `ls $wd/*image.jpg 2> /dev/null | sort -r | head -n 5`;
-    do
-     send_file $file
-    done
+   if [ $move_webdav = "1" ]; then
+    if [ $save_movie = "1" ]; then
+     send_file $video
+    fi
+    if [ $save_picture = "1" ]; then
+     send_file $diff_file
+     for file in `ls $wd/*image.jpg 2> /dev/null | sort -r | head -n 5`; do
+      send_file $file
+     done
+    fi
     clean_wd
    fi
-
   else
    echo "$event Ignoring diff $diff"
   fi
  fi
 
  remove_old_images 10
-
- if [ $state = "close" ];
- then
-  clean_wd
-  rm -rf $cameralock
-  exit
- fi
+ check_for_close
 done;
 
 
