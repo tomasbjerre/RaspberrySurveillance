680e101674449accdf0668cae95a44ed0732cc51
Tomas Bjerre <tomasbjerre@yahoo.com>
2013-07-28 10:09:57 +0100

    Recording video on change

--- sandbox/watch.sh
+++ sandbox/watch.sh
@@ -8,13 +8,17 @@ function handle_exit {
 }
 trap handle_exit SIGHUP SIGINT SIGTERM
 
+cameralock="/tmp/thelock"
+mkdir $cameralock
+
 for (( event=0 ; ; event++ ))
 do
  #echo "Test $event"
  wd="/srv/http/bjerre"
- current="$wd/image-$event.jpg"
- previous="$wd/image-$(expr $event - 1).jpg"
- diff_file="$wd/image-$event-$(expr $event - 1)-diff.jpg"
+ current="$wd/$event-image.jpg"
+ previous="$wd/$(expr $event - 1)-image.jpg"
+ diff_file="$wd/$event-$(expr $event - 1)-diff-image.jpg"
+ video="$wd/$event-video.h264"
  compare_out="/tmp/compareout"
  #echo "Current $current"
  #echo "Previous $previous"
@@ -27,13 +31,25 @@ do
  then
   compare -metric AE -fuzz 10% $current $previous $diff_file 2> $compare_out
   diff=`cat $compare_out`
-  echo "$previous $current $diff"
+  #echo "$previous $current $diff"
+  
+  if [ $diff > 10000 ];
+  then
+   echo "Change: $diff, recording $video"
+   /opt/vc/bin/raspivid -w 640 -h 480 -n -t 20000 -o $video -rot 90
+   echo "Sending {$current, $previous, $diff_file, $video}"   
+   curl -T "{$current,$previous,$diff_file,$video}" "http://tomas:ab12cdab12cd@localhost/owncloud/files/webdav.php/motion/" --http1.0
+   rm -f $wd/*;
+  else
+   echo "No change"
+  fi
  fi
 
  if [ $state = "close" ];
  then
   echo "Clearing $wd in 5 seconds"
   rm -rf $wd/*
+  rm -rf $cameralock
   exit
  fi 
 
